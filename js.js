1e23ae208c8d995502f6a2670d295343f8ec5baf
Marcosv189 <marcos.ferreira189@etec.sp.gov.br>
2023-09-13 17:02:36 -0300

    inicio

---
+++ js.js
@@ -0,0 +1,29 @@
+var lista_de_produtos =[]
+
+while (true ){
+
+
+    var opcao = prompt ( `
+    
+    Bem vindo ao cadastro de produtos 
+
+    1.Cadastrar novos produtos
+    2.Editar produto
+    3.Remover produto
+    4.Vizualizar produtos
+
+    ` )
+
+    if (opcao == 1){
+    alert("passou")
+}else if (opcao == 2){
+  
+}else if (opcao == 3){
+ 
+}else if (opcao == 4){
+  
+}
+else {
+    alert("nenhumas das opçoes foi selecionadas")
+}
+}
